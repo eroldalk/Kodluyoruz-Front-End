9a88ef75be7849093eea3411c93c8b6360e9f39f
eroldalk <eroldalg@gmail.com>
2022-04-01 23:14:13 +0300

    being added

---
+++ Kodluyoroz-Front-End/Csharp proje 101/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen fibonacci seri uzunluğunu giriniz: ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci serisinin toplamı: {0}", fibonacci(length));
+            Console.ReadLine();
+
+        }
+        public static int fibonacci(int x)
+        {
+            if (x <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacci(x - 1) + fibonacci(x - 2);
+            }
+        }
+    }
+}
