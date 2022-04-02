b2138177f7ce3ef96269fd897ab368c575a309ea
eroldalk <eroldalg@gmail.com>
2022-04-03 00:12:37 +0300

    being added

---
+++ Kodluyoroz-Front-End/Csharp proje 101/Üçgenhesaplama/Üçgenhesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace üçgenhesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("üçgenin taban uzunluğunu girin:");
+            int baseSide = int.Parse(Console.ReadLine());
+            üçgen(baseSide);
+
+            Console.ReadLine();
+        }
+        static void üçgen(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    
+}
