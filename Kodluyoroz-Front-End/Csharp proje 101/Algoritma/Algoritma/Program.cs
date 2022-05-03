f89cc5284de4b54c5a2781aabf0eef2dcd7edc7a
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-03 11:26:26 +0300

    add

---
+++ Kodluyoroz-Front-End/Csharp proje 101/Algoritma/Algoritma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "";
+            string[] wordArr;
+            int wordLenght, index;
+            bool isCheck;
+            do
+            {
+                Console.Write("Lütfen kelime ve sayı girin (xxxxx,1): ");
+                word = Console.ReadLine();
+                wordArr = word.Split(',');
+                wordLenght = wordArr[0].Length;
+                index = int.Parse(wordArr[1]);
+                if (wordLenght > index)
+                {
+                    isCheck = true;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı giriş..");
+                    isCheck = false;
+                }
+            } while (isCheck == false);
+
+            System.Console.WriteLine(wordArr[0].Remove(index - 1, 1));
+
+        }
+    }
+}
