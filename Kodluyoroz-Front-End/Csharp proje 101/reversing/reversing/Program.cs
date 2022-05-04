d0c30aa562dd431bae733fffa77f946309e6bc79
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-04 22:51:24 +0300

    add

---
+++ Kodluyoroz-Front-End/Csharp proje 101/reversing/reversing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KarakterTerstenYazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            konsol_islemleri konsol = new Konsol_Islemleri();
+            TerstenYazdırma terstenYazdırma = new TerstenYazdırma();
+            konsol.GirisMesaji();
+            string[] cumleler = konsol.CumleleriAlma();
+            string[] YeniCumleler = terstenYazdırma.TersCevir(cumleler);
+            terstenYazdırma.KelimeleriYazdırma(YeniCumleler);
+        }
+    }
+}
