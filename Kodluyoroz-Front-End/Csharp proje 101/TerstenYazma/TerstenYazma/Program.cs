c6f5646137529cc1de4ac836bb92959ee7199367
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-06 18:22:39 +0300

    Being added

---
+++ Kodluyoroz-Front-End/Csharp proje 101/TerstenYazma/TerstenYazma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("İlk hali : ");
+        string veri = Console.ReadLine();
+
+        var dizi = veri.ToArray();
+
+        var yenidizi =  dizi.Reverse();
+
+        string yeniveri = string.Join("", yenidizi);
+        Console.WriteLine($"Son hali : {yeniveri}");
+
+        
+    }
+}
