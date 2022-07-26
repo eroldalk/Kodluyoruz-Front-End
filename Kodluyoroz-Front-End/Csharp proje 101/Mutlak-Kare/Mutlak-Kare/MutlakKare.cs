ce7955f8a502bb532a51d5d59f764d943bd02e8e
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-26 21:20:09 +0300

    mutlak değer

---
+++ Kodluyoroz-Front-End/Csharp proje 101/Mutlak-Kare/Mutlak-Kare/MutlakKare.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MutlakKareAlma
+{
+    public struct MutlakKare
+    {
+        public void KareAlma(int x)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Sayıları aralarında boşluk bırakarak yazınız: ");
+                    string[] str = Console.ReadLine().Split();
+
+                    List<int> list = new List<int>();
+                    List<int> list2 = new List<int>();
+                    foreach (string s in str)
+                    {
+                        int sayi = int.Parse(s);
+
+                        if (sayi < 67)
+                        {
+                            list.Add(67 - sayi);
+                        }
+                        else
+                        {
+                            list2.Add(sayi - 67);
+                        }
+                    }
+                    int top1 = 0;
+                    foreach (var item in list)
+                        top1 += item;
+
+                    int top2 = 0;
+                    foreach (var item in list2)
+                        top2 += item * item;
+
+                    Console.WriteLine(top1 + " " + top2);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Kodluyoroz-Front-End/Csharp proje 101/Mutlak-Kare/Mutlak-Kare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MutlakKareAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MutlakKare mutlak=new MutlakKare();
+            Console.Write("Lütfen bir sayı giriniz..:");
+            int count =int.Parse(Console.ReadLine());
+            mutlak.KareAlma(count);
+        }
+    }
+}
