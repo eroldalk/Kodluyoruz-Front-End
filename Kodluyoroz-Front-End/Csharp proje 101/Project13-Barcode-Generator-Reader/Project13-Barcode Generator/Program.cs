87f50f9db21c55602c6d5ac9f2278d5e67f82a3f
Erol Dalkılıç <64806208+eroldalk@users.noreply.github.com>
2022-08-08 22:36:40 +0300

    Add files via upload

---
+++ Kodluyoroz-Front-End/Csharp proje 101/Project13-Barcode-Generator-Reader/Project13-Barcode Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using BarcodeLib;
+using Project13_Barcode_Generator;
+
+Barcode barcode =Process.CreateAndSaveBarcode("777");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("11");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("111");
+Console.WriteLine(Process.ReadBarcode(barcode));
+
+barcode = Process.CreateAndSaveBarcode("1111");
+Console.WriteLine(Process.ReadBarcode(barcode));
